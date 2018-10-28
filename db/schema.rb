5f744fc110f9b8783c2f65935c921434c5dbde11
Brian Birdwell <bbirdwell@gmail.com>
2018-10-28 05:45:02 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 30) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
   end
 
   create_table "song", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id"
+    t.integer "genre_id"
+  end
+
+  create_table "songs", force: :cascade do |t|
     t.string "name"
     t.integer "artist_id"
     t.integer "genre_id"
